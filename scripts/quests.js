8d8903531aa1b18b17c4cf2874d79bfc98eb3649
CursedAnubis <cursedanubis@gmail.com>
2015-03-11 11:04:33 -0700

    Peasant Coin image, More questing infrastructure

--- scripts/quests.js
+++ scripts/quests.js
@@ -1,3 +1,5 @@
+var relicFragment = 0;
+
 var inQuest = false;
 var curQuestType = "";
 var questBoost = 0;
@@ -7,7 +9,7 @@ $('#unitSelectPicker').selectpicker({
 });
 
 $('#questSelectPicker').selectpicker({
-	 style: 'btn-success'
+	 style: 'btn-info'
 });
  
  
@@ -32,8 +34,7 @@ var Quest = function(name, description, htmlBoxRef, htmlBarRef, htmlBtnRef, html
 	var $bar = $(document.getElementById(this.htmlBarRef));
 };
 
-Quest.prototype.go = function(){
-	
+Quest.prototype.startQuest = function(){
 	var perComplete = this.percentComplete;
 	var perIncrement = this.percentIncrement;
 	var alert = this.htmlAlertRef;
@@ -47,31 +48,41 @@ Quest.prototype.go = function(){
 	document.getElementById(this.htmlBoxRef).style.display = "block";	
 	
 	$bar = $(document.getElementById(this.htmlBarRef));
+
+	document.getElementById(btn).disabled = true;					//disables the buttons
+	document.getElementById(btn).innerHTML = QuestName + " in progress!";     //Changes button text
+	document.getElementById('questSelectPicker').disabled = true;  //disables picker
+	document.getElementById('unitSelectPicker').disabled = true;   //disables picker
+	document.getElementById('QuestNumSelect').disabled = true;		//disables number select	
+	
 	var progress = setInterval(function() {
 	currWidth = parseInt(this.$bar.attr('aria-valuenow'));
 	maxWidth = parseInt(this.$bar.attr('aria-valuemax'));	
 			
 		//update the progress
-	
 		$bar.width(perComplete +'%');
 		$bar.attr('aria-valuenow',perComplete);
 		$bar.text(perComplete+'%');
 		perComplete = perComplete + perIncrement;
 		this.percentComplete = perComplete;
 		battlePercent = perComplete;
-		
-		document.getElementById(btn).disabled = true;					//disables the buttons
-		document.getElementById(btn).innerHTML = QuestName + " in progress!";     //Changes button text
-		
+				
 	  if (currWidth >= maxWidth){
 		clearInterval(progress);
-			$bar.text("Complete!");
+		$bar.text("Complete!");
 		document.getElementById(box).style.display = "none";			//Hides progress bar box
 		document.getElementById(btn).innerHTML = "Send";                 //Changes button text
-		document.getElementById(btn).disabled = false;					//disables the buttons
+		document.getElementById(btn).disabled = false;					//enables the buttons
+		document.getElementById('questSelectPicker').disabled = false;  //enables picker
+		document.getElementById('unitSelectPicker').disabled = false;   //enables picker
+		document.getElementById('QuestNumSelect').disabled = false;		//enables number select
 //		document.getElementById(alert).style.display = "block";			//Displays alert related to this battle
 //		scroll(alert,500);
 		inQuest = false;
+		$bar.width(0 +'%');
+		$bar.attr('aria-valuenow',0);
+		$bar.text(0+'%');		
+		
 //		setDefeatEvents(EnemyName);
 	  } 
 	}, this.speed);
@@ -80,22 +91,48 @@ Quest.prototype.go = function(){
 
 
 
-function returnPickerSelection(){
-//	console.log($('#unitSelectPicker').selectpicker('selectAll').selected);
+function returnPickerSelection(){			//Need to change function name to something more intuitive
 
- if (checkQuestSelection() == true){
-	 var string = "You send " + $('#QuestNumSelect').val() + " " + $('#unitSelectPicker').selectpicker('val');
-	 if($('#QuestNumSelect').val() > 1){
-		 string = string + "s";
+	 if (checkQuestSelection() == true){
+		 var string = "You send " + $('#QuestNumSelect').val() + " " + $('#unitSelectPicker').selectpicker('val');
+		 if($('#QuestNumSelect').val() > 1){
+			 string = string + "s";
+		 }
+		 string = string + " out on the quest '" + $('#questSelectPicker').selectpicker('val') + "'";
+	  document.getElementById('questAlertString').innerHTML = string;
+	  document.getElementById('sendQuestAlert').style.display = "block";
+//	  alert(string);
+	
+
+	  
+	  switch($('#questSelectPicker').selectpicker('val'))
+	  {
+		 case 'Relic Hunt':
+			if($('#unitSelectPicker').selectpicker('val') == 'Paladin')
+			{
+					RelicHunt.startQuest();
+			}
+			else{
+				alert("You can only send Paladins relic hunting!");
+			}
+		 break;
+		 
+		 case 'Demon Hunt':
+			console.log('Demon hunt');
+		 break;
+		 
+		 case 'Slay Treants':
+			console.log('Slay Treants');
+		 break;
+		 
+		 case 'Help the People':
+			console.log('Help the People');
+		 break;
+	  }
+	//  console.log($('#questSelectPicker').selectpicker('val'));
+	//  console.log($('#unitSelectPicker').selectpicker('val'));
+	//  console.log($('#QuestNumSelect').val()); 
 	 }
-	 string = string + " out on the quest '" + $('#questSelectPicker').selectpicker('val') + "'";
-
-  alert(string);
-  RelicHunt.go();
-  console.log($('#questSelectPicker').selectpicker('val'));
-  console.log($('#unitSelectPicker').selectpicker('val'));
-  console.log($('#QuestNumSelect').val()); 
- }
 };
 
 function checkQuestSelection(){
@@ -154,6 +191,61 @@ function QuestCheckUnitOptions(){
 
 
 //var Quest = function(name, description, htmlBoxRef, htmlBarRef, htmlAlertRef, percentComplete, percentIncrement,speed){
-var relicHuntDesc = "Goblin description placeholder <br><br> You should probably stop them.";
+var relicHuntDesc = "";
 var RelicHunt = new Quest('Relic Hunt', relicHuntDesc, 'QuestProgBarBox', 'QuestProgBar', 'btnQuestGo','goblinDefeatAlert',0,1,500);
 //setEnemyDescription(Goblins, 'btnDescGoblins');
+
+RelicHunt.go = function(){
+	var perComplete = this.percentComplete;
+	var perIncrement = this.percentIncrement;
+	var alert = this.htmlAlertRef;
+	var btn = this.htmlBtnRef;
+	var box = this.htmlBoxRef;
+	var bar = this.htmlBarRef;
+	var QuestName = this.name;
+	var foundRelic = false;
+		
+	inQuest = true;
+	curQuestType = this.name
+	document.getElementById(this.htmlBoxRef).style.display = "block";	
+	
+	$bar = $(document.getElementById(this.htmlBarRef));
+	var progress = setInterval(function() {
+	currWidth = parseInt(this.$bar.attr('aria-valuenow'));
+	maxWidth = parseInt(this.$bar.attr('aria-valuemax'));	
+			
+		//update the progress
+		$bar.width(perComplete +'%');
+		$bar.attr('aria-valuenow',perComplete);
+		$bar.text(perComplete+'%');
+		perComplete = perComplete + perIncrement;
+		this.percentComplete = perComplete;
+		battlePercent = perComplete;
+		
+		document.getElementById(btn).disabled = true;					//disables the buttons
+		document.getElementById(btn).innerHTML = QuestName + " in progress!";     //Changes button text
+		document.getElementById('questSelectPicker').disabled = true;  //disables picker
+		document.getElementById('unitSelectPicker').disabled = true;   //disables picker
+		document.getElementById('QuestNumSelect').disabled = true;		//disables number select
+		
+	  if (currWidth >= maxWidth){
+		clearInterval(progress);
+			$bar.text("Complete!");
+		document.getElementById(box).style.display = "none";			//Hides progress bar box
+		document.getElementById(btn).innerHTML = "Send";                 //Changes button text
+		document.getElementById(btn).disabled = false;					//enables the buttons
+		document.getElementById('questSelectPicker').disabled = false;  //enables picker
+		document.getElementById('unitSelectPicker').disabled = false;   //enables picker
+		document.getElementById('QuestNumSelect').disabled = false;		//enables number select
+//		document.getElementById(alert).style.display = "block";			//Displays alert related to this battle
+//		scroll(alert,500);
+		inQuest = false;
+		
+		$bar.width(0 +'%');
+		$bar.attr('aria-valuenow',0);
+		$bar.text(0+'%');		
+//		setDefeatEvents(EnemyName);
+	  } 
+	}, this.speed);
+	return true;
+}
