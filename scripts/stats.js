5d3b7eb0061e7c302b38353f8d73aaac993d9c6e
CursedAnubis <cursedanubis@gmail.com>
2015-03-25 15:15:07 -0700

    Added Rudimentary achievement page

---
+++ scripts/stats.js
@@ -0,0 +1,110 @@
+//Holy Wars Lifetime Variables
+var statResetted = 0;
+
+var statGoldCollected = 0;
+var statSelfGoldCollected = 0;
+var statTotalSelfGoldCollected = 0;
+var statTotalGoldCollected = 0;
+
+var statWoodCollected = 0;
+var statSelfWoodCollected = 0;
+var statTotalWoodCollected = 0;
+var statTotalSelfWoodCollected = 0;
+
+var statIronCollected = 0;
+var statTotalIronCollected = 0;
+
+var statSilverCollected = 0;
+var statTotalSilverCollected = 0;
+
+var statPaperCrafted = 0;
+var statSelfPaperCrafted = 0;
+var statTotalPaperCrafted = 0;
+var statTotalSelfPaperCrafted = 0;
+
+var statTomesCrafted = 0;
+var statSelfTomesCrafted = 0;
+var statTotalTomesCrafted = 0;
+
+var statFaithCollected = 0;
+var statTotalFaithCollected = 0;
+
+var statSoulsCollected = 0;
+var statTotalSoulsCollected = 0;
+
+var statPeasantsHired = 0;
+var statSelfPeasantsHired = 0;
+var statTotalSelfPeasantsHired = 0;
+var statTavernPeasantsHired = 0;
+var statTotalTavernPeasantsHired = 0;
+var statTotalPeasantsHired = 0;
+
+var statMinersHired = 0;
+var statSelfMinersHired = 0;
+var statTotalSelfMinersHired = 0;
+var statTavernMinersHired = 0;
+var statTotalTavernMinersHired = 0;
+var statTotalMinersHired = 0;
+
+var statLumberjacksHired = 0;
+var statSelfLumberjacksHired = 0;
+var statTotalSelfLumberjacksHired = 0;
+var statTavernLumberjacksHired = 0;
+var statTotalTavernLumberjacksHired = 0;
+var statTotalLumberjacksHired = 0;
+
+var statPagesTrained = 0;
+var statTotalPagesTrained = 0;
+
+var statSquiresTrained = 0;
+var statTotalSquiresTrained = 0;
+
+var statKnightsTrained = 0;
+var statTotalKnightsTrained = 0;
+
+var statPaladinsTrained = 0;
+var statTotalPaladinsTrained = 0;
+
+var statAcolytesRecruited = 0;
+var statTotalAcolytesRecruited = 0;
+
+var statPriestsTrained = 0;
+var statTotalPriestsTrained = 0;
+
+var statBishopsTrained = 0;
+var statTotalBishopsTrained = 0;
+
+var statShadesSummoned = 0;
+var statTotalShadesSummoned = 0;
+
+var statAspectsTrained = 0;
+var statTotalAspectsTrained = 0;
+
+var statAngelsSummoned = 0;
+var statTotalAngelsSummoned = 0;
+
+var statTavernsBuilt = 0;
+var statPaperMillsBuilt = 0;
+
+//Battle Stats
+var statTotalEnemiesDefeated = 0;
+var statEnemiesDefeated = 0;
+var statUnitsKilledInBattle = 0;
+var statTotalUnitsKilledInBattle = 0;
+var statTotalGoldStolen = 0;
+var statTotalPeasantsKilled = 0;
+var statTotalMinersKilled = 0;
+var statTotalIronStolen = 0;
+var statTotalSilverStolen = 0;
+var statTotalUnitsSeduced = 0;
+
+var statManaGained = 0;
+var statTotalManaGained = 0;
+var statManaUsed = 0;
+var statTotalManaUsed = 0;
+
+var statCastedFireBall = 0;
+var statTotalCastedFireBall = 0;
+
+var statCastedTimeSkip = 0;
+var statTotalCastedTimeSkip = 0;
